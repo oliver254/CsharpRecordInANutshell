7eadf262cd51e9ef4320e0ca71b4e3767380c89e
oliver254 <oliver254@hotmail.fr>
2021-11-14 20:00:17 +0100

    Ajoutez des fichiers projet.

---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Record In a Nutshell
+
+## Credits
+
+- T. Mads ["C# 9.0 on the record"](https://devblogs.microsoft.com/dotnet/c-9-0-on-the-record/).
---
+++ test/CsharpRecordInANutshell.Tests/Csharp9OnTheRecordTests.cs
@@ -0,0 +1,94 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CsharpRecordInANutshell.Tests;
+
+public class Csharp9OnTheRecordTests
+{
+    // https://devblogs.microsoft.com/dotnet/c-9-0-on-the-record/
+
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+
+    [Fact]
+    public void Class_is_mutable()
+    {
+        var person = new Person { FirstName = "Harold", LastName = "Triggerman" };
+
+        person.LastName = "Quail";
+
+        person.LastName.Should().Be("Quail");
+    }
+
+    public class InitAccessorPerson
+    {
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+    }
+
+    [Fact]
+    public void Init_accessor_introduces()
+    {
+        var person = new InitAccessorPerson { FirstName = "Harold", LastName = "Triggerman" };
+
+        //person.LastName = "Quail": // error
+    }
+
+    public class InitAccessorAndReadOnlyFieldsPerson
+    {
+        private readonly string firstName = "<unknown>";
+        private readonly string lastName = "<unknown>";
+
+        public string FirstName
+        {
+            get => firstName;
+            init => firstName = (value ?? throw new ArgumentNullException(nameof(FirstName)));
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            init => lastName = (value ?? throw new ArgumentNullException(nameof(LastName)));
+        }
+
+        [Fact]
+        public void Init_accessor_and_readonly_fields_introduce()
+        {
+            var person = new InitAccessorAndReadOnlyFieldsPerson { FirstName = "Harold", LastName = "Triggerman" };
+        }
+
+        public record RecordPerson
+        {
+            public string? FirstName { get; init; }
+            public string? LastName { get; init; }
+        }
+
+        [Fact]
+        public void Record_is_immutable_like_a_value()
+        {
+            var person = new RecordPerson { FirstName = "Harold", LastName = "Triggerman" };
+            var otherPerson = person with { LastName = "Quail" };
+
+            ReferenceEquals(person, otherPerson).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Record_is_immutable_like_a_value2()
+        {
+            var person = new RecordPerson { FirstName = "Harold", LastName = "Quail" };
+            var otherPerson = new RecordPerson { FirstName = "Harold", LastName = "Quail" };
+
+            ReferenceEquals(person, otherPerson).Should().BeFalse();
+
+            person.GetHashCode().Equals(otherPerson.GetHashCode()).Should().BeTrue();
+
+            (person == otherPerson).Should().BeTrue();
+
+            person.Equals(otherPerson).Should().BeTrue();
+        }
+    }
+}
